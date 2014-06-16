16e0d08e57a1a4fb876754d123e544b20a94bfd0
thelamacmdr <thelamacmdr@gmail.com>
2014-06-15 23:11:52 -0700

    Add new files

---
+++ axapp/src/main/java/com/lamapress/animeexpo2014/axapp/HomeFragment.java
@@ -0,0 +1,15 @@
+package com.lamapress.animeexpo2014.axapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * @author Anthony Lame
+ * @version 1.0
+ */
+public class HomeFragment extends Fragment {
+
+    public void onCreateView(){
+
+    }
+}

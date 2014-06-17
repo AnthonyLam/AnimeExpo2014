6e0d1d5b515e1eea9d8ef68098c99666e6b46686
thelamacmdr <thelamacmdr@gmail.com>
2014-06-16 23:51:04 -0700

    Initialize HomeFragment

--- axapp/src/main/java/com/lamapress/animeexpo2014/axapp/HomeFragment.java
+++ axapp/src/main/java/com/lamapress/animeexpo2014/axapp/HomeFragment.java
@@ -1,7 +1,14 @@
 package com.lamapress.animeexpo2014.axapp;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cengalabs.flatui.FlatUI;
 
 /**
  * @author Anthony Lame
@@ -9,7 +16,24 @@ import android.support.v4.app.FragmentStatePagerAdapter;
  */
 public class HomeFragment extends Fragment {
 
-    public void onCreateView(){
+    ViewPager imagePager;
 
+    public static HomeFragment newInstance(int sectionNumber){
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public void onDestroy(){
+        super.onDestroy();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        FlatUI.initDefaultValues(getActivity());
+        FlatUI.setDefaultTheme(FlatUI.CANDY);
     }
 }

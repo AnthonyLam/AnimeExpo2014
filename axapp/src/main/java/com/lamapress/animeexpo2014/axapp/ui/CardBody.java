521280fba37c75f9fac4f9f151664d98eb7359c8
thelamacmdr <thelamacmdr@gmail.com>
2014-06-29 18:24:59 -0700

    Implement Schedule view using shared preferences

--- axapp/src/main/java/com/lamapress/animeexpo2014/axapp/ui/CardBody.java
+++ axapp/src/main/java/com/lamapress/animeexpo2014/axapp/ui/CardBody.java
@@ -1,15 +1,19 @@
 package com.lamapress.animeexpo2014.axapp.ui;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lamapress.animeexpo2014.axapp.R;
 import com.lamapress.animeexpo2014.axapp.core.Panel;
+import com.lamapress.animeexpo2014.axapp.sqlite_helper.DatabaseHelper;
 
 import java.util.GregorianCalendar;
 
@@ -19,43 +23,63 @@ import it.gmariotti.cardslib.library.internal.Card;
  * Card type for panels
  */
 public class CardBody extends Card {
-        ImageView m_Rectangle;
-        TextView m_PanelName;
-        TextView m_PanelDescription;
-        TextView m_PanelTime;
-        TextView m_PanelLocation;
-        CheckBox m_Favorited;
+    ImageView m_Rectangle;
+    TextView m_PanelName;
+    TextView m_PanelDescription;
+    TextView m_PanelTime;
+    TextView m_PanelLocation;
+    CheckBox m_Favorited;
 
-        Panel panel;
-        GregorianCalendar date;
+    Panel panel;
+    GregorianCalendar date;
 
-        public CardBody(Context context, Panel panel){
-            super(context, R.layout.card_panel_inner);
-            this.panel = panel;
-        }
+    SharedPreferences pref;
+    Context context;
 
-        @Override
-        public void setupInnerViewElements(ViewGroup vg, View v){
-            m_Rectangle = (ImageView) v.findViewById(R.id.colorBorder);
-            m_PanelName = (TextView) v.findViewById(R.id.card_panel_name);
-            m_PanelDescription = (TextView) v.findViewById(R.id.card_main_inner_simple_title);
-            m_PanelTime = (TextView) v.findViewById(R.id.card_time);
-            m_PanelLocation = (TextView) v.findViewById(R.id.card_panel_location);
-            m_Favorited = (CheckBox) v.findViewById(R.id.favorited);
 
-            m_Rectangle.setBackgroundColor(Color.rgb(255, 0, 0));
+    public CardBody(Context context, Panel panel){
+        super(context, R.layout.card_panel_inner);
+        this.context = context;
+        this.panel = panel;
+    }
 
-            // Fill view with data
-            m_PanelName.setText(panel.getM_sPanelName());
-            m_PanelDescription.setText(panel.getM_sPanelDescription());
+    @Override
+    public void setupInnerViewElements(ViewGroup vg, View v){
+        m_Rectangle = (ImageView) v.findViewById(R.id.colorBorder);
+        m_PanelName = (TextView) v.findViewById(R.id.card_panel_name);
+        m_PanelDescription = (TextView) v.findViewById(R.id.card_main_inner_simple_title);
+        m_PanelTime = (TextView) v.findViewById(R.id.card_time);
+        m_PanelLocation = (TextView) v.findViewById(R.id.card_panel_location);
+        m_Favorited = (CheckBox) v.findViewById(R.id.favorited);
 
-            date = new GregorianCalendar();
-            date.setTimeInMillis(panel.m_PanelTime);
+        m_Rectangle.setBackgroundColor(Color.rgb(255, 0, 0));
 
-            m_PanelTime.setText(date.getTime().toString());
-            m_PanelLocation.setText(panel.getM_sPanelRoom());
-            m_Favorited.setChecked(panel.getFavorited());
+        // Fill view with data
+        m_PanelName.setText(panel.getM_sPanelName());
+        m_PanelDescription.setText(panel.getM_sPanelDescription());
 
-        }
+        date = new GregorianCalendar();
+        date.setTimeInMillis(panel.m_PanelTime);
+
+        m_PanelTime.setText(date.getTime().toString());
+        m_PanelLocation.setText(panel.getM_sPanelRoom());
+
+        pref = context.getSharedPreferences("FAVORITE",0);
+
+        m_Favorited.setChecked(pref.contains(panel.getId()));
+
+        m_Favorited.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    pref.edit().putBoolean(panel.getId(), true).apply();
+                }
+                else if(pref.contains(panel.getId())){
+                    pref.edit().remove(panel.getId()).apply();
+                }
+            }
+        });
+    }
+
 
 }
